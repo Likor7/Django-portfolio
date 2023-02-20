22a081f6c2bf3938a886025f274b990b67b38392
Likor7 <sov30082003@gmail.com>
2023-02-20 14:31:57 +0200

    Added setup for tests and .evn example file.

--- apps/portfolio/tests.py
+++ apps/portfolio/tests.py
@@ -1,3 +1,28 @@
+import tempfile
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+MEDIA_ROOT = tempfile.mkdtemp()
+
+User = get_user_model()
+
+
+class UserMixin(object):
+    @staticmethod
+    def _create_user(number: int):
+        user = User.objects.create(
+            username=f"test_{number}", email=f"test_{number}@example.com"
+        )
+        user.set_password("testPassword")
+        user.save()
+        return user
+
+
+class ImageTestCase(TestCase, UserMixin):
+    def setup(self) -> None:
+        self.user = self._create_user(1)
+
+        self.login_url = reverse("accounts:login")
+        self.image_list_url = reverse("portfolio:image-list")
+        self.image_detail_url = reverse("portfolio:image-detail", kwargs={"pk": 1})
